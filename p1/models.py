074086008b8c8e3be2080f1860d2a023250f8dc5
Abhilash Mhaisne <55abhilash@openmailbox.org>
2016-12-25 13:31:11 +0530

    Created models (classes)

--- p1/models.py
+++ p1/models.py
@@ -12,6 +12,43 @@ wheel = salt.wheel.WheelClient(opts)
 
 # Create your models here.
 
+class machine():
+    machine_id = ""
+    os = ""
+    arch = ""
+    is_live = False
+    in_use = False
+
+class task():
+    selected_machines = list()
+    task = -1
+    task_status = -1
+
+    def generate_input(self):
+        # Generate the input format for the particular task
+        tmp = 0
+
+    def run(self, selected_machines, inp):
+        # cmd.run will take place here
+        tmp = 0
+    return_status = list()
+
+    def create_output(self):
+        # Success status received from minions for this task
+        tmp = 0
+    
+    # Complete output strings from each of the minions
+    individual_outputs = dict()
+    def fill_individual_outputs(self, selected_machines):
+        tmp = 0
+    def display_individual_output(self, selected_machines):
+        tmp = 0
+
+class connect():
+    selected_machine_ids = list()
+    def redirect_to_task_page(self, selected_machine_ids):
+        tmp = 0
+
 class registrations():
     def show_pending_registrations(self):
         return wheel.cmd('key.list_all')['minions_pre']
