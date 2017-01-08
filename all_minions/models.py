8f21a224807e3f9c1b16eb703e226a2158f75e7b
Abhilash Mhaisne <55abhilash@openmailbox.org>
2017-01-08 15:16:00 +0530

    Added OS and IP Address columns to all_minions table. Fixed indentation of html written through jquery.

--- all_minions/models.py
+++ all_minions/models.py
@@ -7,13 +7,14 @@ import salt
 import salt.config
 import salt.wheel
 import salt.runner
+import salt.loader
 
 import p1.models
 
 opts = salt.config.master_config('/etc/salt/master')
 wheel = salt.wheel.WheelClient(opts)
 run = salt.runner.RunnerClient(opts)
-
+grains = salt.loader.grains(opts)
 # Create your models here.
 
 class registrations(p1.models.registrations):
@@ -22,10 +23,14 @@ class registrations(p1.models.registrations):
         minion_status = dict()
         salt_run_minion_status = run.cmd('manage.status')
         for minion in all:
+            minion_status[minion] = list()
+            minion_status[minion].append(grains['os'] + " " + grains['osrelease'] + " " + grains['oscodename'])
+            print "DEBUG : OS INFO :- " + minion_status[minion][0]
+            minion_status[minion].append(grains['ipv4'][0]) #The list returned by ipv4 contains the public ip at 0th index, localhost at 1st and other ips if present at later indexes
             if(minion in salt_run_minion_status['down']):
-                minion_status[minion] = "Down"
+                minion_status[minion].append("Down")
             else:
-                minion_status[minion] = "Up"
+                minion_status[minion].append("Up")
         return minion_status
 
     def delete_ids(self, machine_ids):
