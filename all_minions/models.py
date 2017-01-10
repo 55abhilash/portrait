077e5b93ebf51062b3ee98402871ae40f9a0731e
Abhilash Mhaisne <55abhilash@openmailbox.org>
2017-01-10 16:00:07 +0530

    Ran the client.cmd_async instead of client.cmd. This will let the job run and return job id immediately. Right now, the job is being run from all_minions. This is to be changed to run in from accept_ids and then pass the job id properly to all_minions.

--- all_minions/models.py
+++ all_minions/models.py
@@ -13,6 +13,8 @@ import salt.client
 import p1.models
 from p1.models import machine
 
+from pending_registrations.models import registrations as pr_registrations
+
 opts = salt.config.master_config('/etc/salt/master')
 wheel = salt.wheel.WheelClient(opts)
 run = salt.runner.RunnerClient(opts)
@@ -25,12 +27,43 @@ class registrations(p1.models.registrations):
         all = wheel.cmd('key.list_all')['minions']
         minion_status = dict()
         salt_run_minion_status = run.cmd('manage.status')
+                                    
         for minion in all:
             minion_status[minion] = list()
+            
+            grains_info_job = client.cmd_async('*', 'grains.items')
+            
+            #print "grains_info_job is : "
+            #print grains_info_job 
+            grains_info_data = client.get_cli_returns(grains_info_job, minion)
+            
+            #print "grains info is "
+            #print grains_info_data
+            
+            grains_info = {}
+            for mid in list(grains_info_data):
+                if(minion in dict(mid)):
+                    # Good to proceed
+                    #print "DEBUG : mid = "
+                    #print mid
+                    #print "DEBUG : dict(mid)[minion]['ret'] = "
+                    #print dict(mid)[minion]['ret']
+                    grains_info[minion] = dict(mid)[minion]['ret']
+                    
+            #print "grains info dict is "
+            #print grains_info
+           
+            os_info = grains_info[minion]['os'] + ' ' + grains_info[minion]['osrelease'] + ' ' + grains_info[minion]['oscodename']
+            ipv4 = grains_info[minion]['ipv4'][0]
+            mac = p1.models.machine(machine_id=minion, os=os_info, arch=grains_info[minion]['osarch'], ip=ipv4)
+            mac.save()
             mac = machine.objects.get(machine_id=minion)
-            minion_status[minion].append(mac.os)
+            minion_status[minion].append(os_info)
+            # If minion is down, use their latest ip saved in the database
+            # If up, run grains command to get latest ip and also save it in 
+            # the database
             if(minion in salt_run_minion_status['down']):
-                minion_status[minion].append(mac.ip)
+                minion_status[minion].append(ipv4)
                 minion_status[minion].append("Down")
             else:
                 ipv4 = client.cmd(minion, 'grains.item', ['ipv4'])[minion]['ipv4'][0]
--- pending_registrations/models.py
+++ pending_registrations/models.py
@@ -23,38 +23,17 @@ e = threading.Event()
 # Create your models here.
 
 class registrations(p1.models.registrations):
-    def wait_till_info_available(self, machine):
-            while True:
-                time.sleep(5)
-                client = salt.client.LocalClient()
-                print "machine = " + machine
-                if machine in client.cmd(machine, 'grains.item', ['os']) and machine in client.cmd(machine, 'grains.item', ['osrelease']) and machine in client.cmd(machine, 'grains.item', ['oscodename']):
-                    e.set()
-                    return 
+    grains_info_job = '' 
     def show_pending_registrations(self):
         return wheel.cmd('key.list_all')['minions_pre']
     def accept_ids(self, machine_ids):
         for machine in machine_ids:
             machine_orig = machine.split()
             machine = machine.split()[0].split('"')[1]
-            print "machine.split()[0] = " + machine
             wheel.cmd('key.accept', machine_orig)
-
-            thread.start_new_thread(self.wait_till_info_available, (machine,))
-            e.clear()
-            e.wait()
-            client = salt.client.LocalClient()
-            
-            os_info = client.cmd(machine, 'grains.item', ['os'])[machine]['os'] + ' ' + client.cmd(machine, 'grains.item',['osrelease'])[machine]['osrelease'] + ' ' + client.cmd(machine, 'grains.item', ['oscodename'])[machine]['oscodename']
-            ipv4 = client.cmd(machine, 'grains.item', ['ipv4'])[machine]['ipv4'][0]
-
-            mac = p1.models.machine(machine_id=machine, os=os_info, arch=client.cmd(machine, 'grains.item', ['osarch'])[machine]['osarch'], ip=ipv4)
-            mac.save()
     def reject_ids(self, machine_ids):
         for machine in machine_ids:
             wheel.cmd('key.reject', machine.split())
 
-#class db_updater():
- #   def update(self, ):
 
 
